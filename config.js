ab31369145c2cfab6eb1ea3b0a87f789fb104226
shivraj <shivrajpoojari1996@gmail.com>
2026-05-12 17:30:40 +0530

    updated issues

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const MMR_CONFIG = {
   GOOGLE_SHEET_ID: '2PACX-1vS5d3dmhihCWDCLl5ehT88ae3S4ezSP8Cv3VDqqGj1Qo06uFhjA7A_SlXx6RIRmB5NxjbG3nFMHzAxv',
   SHEET_NAME: 'Sheet1',
-  CACHE_MINUTES: 30,
+  CACHE_MINUTES: 0, // Set to 0 for instant updates during testing (normally 30)
   USE_FALLBACK: true
 };
 
